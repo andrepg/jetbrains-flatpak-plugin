d54d0e91d28f4150c8114885b9890c3582fc8b51
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-08-13 14:29:18 -0300

    refactor: rename NotImplementedException to FeatureNotImplementedException
    The old name was a deprecated alias of Kotlin's built-in NotImplementedError and shadowed it in this package.

---
+++ src/main/kotlin/io/github/andrepg/shared/exceptions/FeatureNotImplementedException.kt
@@ -0,0 +1,6 @@
+package io.github.andrepg.shared.exceptions
+
+/**
+ * Thrown to indicate that a feature is recognized but not yet implemented.
+ */
+class FeatureNotImplementedException : RuntimeException("Not implemented yet")
